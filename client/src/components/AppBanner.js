67360c4a9a2bbd6c80b566ed07779319af572a05
elvenli54 <90856089+elvli@users.noreply.github.com>
2023-11-06 02:44:29 -0500

    dcds

--- client/src/components/AppBanner.js
+++ client/src/components/AppBanner.js
@@ -5,7 +5,6 @@ export default function AppBanner() {
     return(
         <nav className="navbar navbar-expand-lg navbar-dark bg-dark app-banner">
           <p className="navbar-brand">GeoCraft</p>
-          <button type="button" className="btn btn-primary">Click Here (Test)</button>
         </nav>
     )
 }
